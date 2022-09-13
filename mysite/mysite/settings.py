539a6c6a23f4fc88b2372462efc871caa864ad77
owolabi-develop <owolabidevelop84@gmail.com>
2022-09-12 16:28:05 -0800

    footer and menu color changes

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 AUTH_USER_MODEL = 'blog.User'
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 CSRF_COOKIE_SECURE = True
